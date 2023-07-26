a3efec2cd2d5a3d678110666d07538909f1fcbf1
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-25 21:06:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import express from 'express';
 
 import cron from 'node-cron';
 
-
-
 let rssLink = 'https://carlosmv.hashnode.dev/rss.xml';
 
 
@@ -16,6 +14,7 @@ cron.schedule("*/15 * * * * *", function () {
     console.log("running a task every 15 seconds");
   });
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -24,6 +23,9 @@ app.get('/feed',async  (req, res) => {
   let lastEntry = await LastEntry(rssLink)
   res.send(lastEntry)
 })
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
